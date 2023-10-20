9c09e57e938c96082eabd81653f9377fcedcceca
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-20 14:16:02 -0300

    Adicionar arquivos de projeto.

---
+++ ClasseExercicio2/Funcionario.cs
@@ -0,0 +1,6 @@
+﻿namespace ClasseExercicio2 {
+    internal class Funcionario {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ ClasseExercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using ClasseExercicio2;
+using System.Globalization;
+
+Funcionario func1 = new Funcionario();
+Funcionario func2 = new Funcionario();
+
+Console.WriteLine("Dados do primeiro funcionário:");
+Console.Write("Nome: ");
+func1.Nome = Console.ReadLine();
+Console.Write("Salário: ");
+func1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Dados do segundo funcionário:");
+Console.Write("Nome: ");
+func2.Nome = Console.ReadLine();
+Console.Write("Salário: ");
+func2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double salarioMedio = (func1.Salario + func2.Salario) / 2.0;
+
+Console.WriteLine("Salário médio = " + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
